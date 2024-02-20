f95c0c806f040674e63253a637378fe64dd78bfc
JoyceFatima <joycecosta453@hotmail.com>
2024-02-20 13:57:12 -0300

    chore: add Variantes & Slots API

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,5 +1,6 @@
 import { LogOut } from 'lucide-react'
 import Image from 'next/image'
+import { Button } from './Button'
 
 export default function Profile() {
   return (
@@ -21,9 +22,9 @@ export default function Profile() {
         </span>
       </div>
 
-      <button type="button" className="ml-auto rounded-md p-2 hover:bg-zinc-50">
+      <Button variant="ghost" type="button">
         <LogOut className="h-5 w-5 text-zinc-500" />
-      </button>
+      </Button>
     </div>
   )
 }
